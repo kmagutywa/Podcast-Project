09db3ba356e82add5b7f4c7fc7a56d2dc9fcea18
Kwakhanya <kmagutywa@gmail.com>
2022-11-15 16:11:11 +0200

    Update api.js file

--- api.js
+++ api.js
@@ -8,9 +8,35 @@
  * @property {string} updated
  */
 
+/**
+ * @typedef {object} episode
+ * @property {number} episode
+ * @property {string} description
+ * @property {string} title
+ * @property {string} file
+ */
+
+ /**
+ * @typedef {object} season
+ * @property {number} season
+ * @property {string} title
+ * @property {string} image
+ * @property {episode[]} episodes
+ */
+
+  /**
+ * @typedef {object} show
+ * @property {string} id
+ * @property {string} title
+ * @property {season []} seasons
+ * @property {string} image
+ * @property {string} genres
+ * @property {string} updated
+ */
 const listHtml = document.querySelector('#list')
 
-const getData = async() => {
+const getAllPodcasts = async() => {
+    listHtml.innerHTML= `Loading...`
     const response = await fetch('https://podcast-api.netlify.app/shows')
     
     if (!response.ok) {
@@ -24,17 +50,73 @@ const getData = async() => {
 
     let newHtml = ''
 
-    for (const {title, seasons } of data) {
+    for (const {id,title, seasons } of data) {
         newHtml=  `
         ${newHtml} 
            <li>
-               <button>${title} </button>
+               <button data-preview-button="${id}">${title} </button>
                <span>(${seasons})</span>
           </li> 
         `
     }
-listHtml.innerHTML= newHtml
+    
+    listHtml.innerHTML= newHtml
 
 }
+
+
+/**
+ * @param {string} id 
+ */
+const getSinglePodcast = async (id) => {
+    listHtml.innerHTML= `Loading...`
+    const response = await fetch(`https://podcast-api.netlify.app/id/${id}`)
+
+    if (!response.ok){
+        listHtml.innerHTML = 'Something went wrong'
+    }
+
+    /**
+     * @type {show}
+     */
+
+    const data = await response.json()
+
+    let seasonsList = ''
     
-    getData()
+    for (const {image, title} of data.seasons){
+        
+        seasonsList = `
+        ${seasonsList}
+        
+        <li>
+            <img src = "${image}" width = "100" height = "100">
+            ${title}
+        </li>
+        `
+    }
+
+    listHtml.innerHTML = `
+    <button data-action="back">◀️ BACK </button>
+    <h2>${data.title}<h2>
+
+    <ul>
+        ${seasonsList}
+    </ul>
+    `
+}
+document.body.addEventListener('click', (event)=>{
+    const {previewButton, action} = event.target.dataset
+
+    if (action && action === 'back') {
+        getAllPodcasts()
+
+        return
+    }
+    if (!previewButton) return
+
+    
+    getSinglePodcast(previewButton)
+})
+    
+    getAllPodcasts()
